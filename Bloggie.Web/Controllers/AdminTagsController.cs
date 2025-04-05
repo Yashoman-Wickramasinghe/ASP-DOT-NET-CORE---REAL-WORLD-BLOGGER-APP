30391252b36632f46f1e0ff2d3901ed971d9a516
Yasho Wickramasinghe <yashoman0608@gmail.com>
2025-04-05 22:57:35 +1100

    Add project files.

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,116 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BloggieDbContext _bloggieDbContext;
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            _bloggieDbContext = bloggieDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping the AddTagRequest to the Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            _bloggieDbContext.Tags.Add(tag);
+            _bloggieDbContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            // Use BloggieDbContext to read the tags
+            var tags = _bloggieDbContext.Tags.ToList();
+
+            return View(tags);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid Id) 
+        {
+            // Use BloggieDbContext to read the tag by Id
+            var tag = _bloggieDbContext.Tags.FirstOrDefault(t => t.Id == Id);
+
+            if (tag != null)
+            {
+                // bind the tag to the EditTagRequest ViewModel
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+                return View(editTagRequest);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest) 
+        { 
+            // Convert back into the tag to Main Domain Model
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+
+            // Validate the tag
+            var existingTag = _bloggieDbContext.Tags.Find(tag.Id);
+
+            if(existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                // save the changes
+                _bloggieDbContext.SaveChanges();
+
+                // show success notification
+                return RedirectToAction("Edit", new { id = editTagRequest.Id });
+            }
+            // show success error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id});
+        }
+
+        [HttpPost]
+        public IActionResult Delete(EditTagRequest editTagRequest)
+        {
+            var tag = _bloggieDbContext.Tags.Find(editTagRequest.Id);
+
+            if (tag != null) 
+            {
+                _bloggieDbContext.Tags.Remove(tag);
+                _bloggieDbContext.SaveChanges();
+
+                // show success notification
+                return RedirectToAction("List");
+            }
+
+            // show error notification
+            return View("Edit", new { id = editTagRequest.Id });
+        }
+    }
+}
---
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -0,0 +1,12 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+        //This Tag has multiple BlogPosts
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
