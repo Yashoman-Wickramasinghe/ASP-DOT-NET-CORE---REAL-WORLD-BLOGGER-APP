28584408d52a16a72d887f501fd45adf09571c54
Yasho Wickramasinghe <yashoman0608@gmail.com>
2025-04-13 20:04:54 +1000

    Displaying Tags and All saved blogs on HomePage. Each blog can read by clicking the button "Read More".

--- Bloggie.Web/Controllers/HomeController.cs
+++ Bloggie.Web/Controllers/HomeController.cs
@@ -1,21 +1,39 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Bloggie.Web.Models;
+using Bloggie.Web.Repositories;
+using Bloggie.Web.Models.ViewModels;
 
 namespace Bloggie.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IBlogPostRepository blogPostRepository;
+    private readonly ITagRepository tagRepository;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository, ITagRepository tagRepository)
     {
         _logger = logger;
+        this.blogPostRepository = blogPostRepository;
+        this.tagRepository = tagRepository;
     }
 
-    public IActionResult Index()
+    public async Task <IActionResult> Index()
     {
-        return View();
+        // getting all blogs
+        var blogPost = await blogPostRepository.GetAllAsync();
+
+        // get all tags
+        var tags = await tagRepository.GetAllAsync();
+
+        var model = new HomeViewModel
+        {
+            BlogPosts = blogPost,
+            Tags = tags
+        };
+
+        return View(model);
     }
 
     public IActionResult Privacy()
