ff497c44c50ef11714adc85c0e229be3166e7d14
Yasho Wickramasinghe <yashoman0608@gmail.com>
2025-04-17 15:55:16 +1000

    Registered users can like and comment on each blog posts.

---
+++ Bloggie.Web/Controllers/BlogPostLikeController.cs
@@ -0,0 +1,45 @@
+﻿using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Bloggie.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostLikeController : ControllerBase
+    {
+        private readonly IBlogPostLikeRepositroy blogPostLikeRepositroy;
+
+        public BlogPostLikeController(IBlogPostLikeRepositroy blogPostLikeRepositroy)
+        {
+            this.blogPostLikeRepositroy = blogPostLikeRepositroy;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> AddLike([FromBody] AddLikeRequest addLikeRequest)
+        {
+            var model = new BlogPostLike
+            {
+                BlogPostId = addLikeRequest.BlogPostId,
+                UserId = addLikeRequest.UserId
+            };
+
+            await blogPostLikeRepositroy.AddLikeForBlog(model);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{blogPostId:Guid}/totalLikes")]
+        public async Task<IActionResult> GetTotalLikeForBlog([FromRoute] Guid blogPostId)
+        {
+            var totalLikes = await blogPostLikeRepositroy.GetTotalLikes(blogPostId);
+
+            return Ok(totalLikes);
+        }
+    }
+}
---
+++ Bloggie.Web/Repositories/IBlogPostLikeRepositroy.cs
@@ -0,0 +1,12 @@
+﻿using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Repositories
+{
+    public interface IBlogPostLikeRepositroy
+    {
+        Task<int> GetTotalLikes(Guid blogPostId);
+
+        Task<IEnumerable<BlogPostLike>> GetLikesForBlog(Guid blogPostId);
+        Task<BlogPostLike> AddLikeForBlog(BlogPostLike blogPostLike);
+    }
+}
